2801702951b815618e93c7d8e4cf912455935bce
Sherry Zhao <sherrytxz06@gmail.com>
2021-11-26 16:06:22 -0500

    purchase history setup

--- pharmacy_project/pharmacy_project/models.py
+++ pharmacy_project/pharmacy_project/models.py
@@ -17,3 +17,71 @@ class Employee(models.Model):
     class Meta:
         db_table = 'employee'
 
+class Doctor(models.Model):
+    d_id = models.IntegerField(primary_key=True)
+    d_name = models.CharField(max_length=50, null = False)
+    d_hospital = models.CharField(max_length=100, null = False)
+
+    class Meta:
+        db_table = 'doctor'
+
+class Manufacturer(models.Model):
+    f_id = models.IntegerField(primary_key=True)
+    f_name = models.CharField(max_length=50, null = False)
+    f_phone_num = models.CharField(max_length=10)
+    f_address = models.CharField(max_length=100)
+
+    class Meta:
+            db_table = 'manufacturer'
+
+
+class Customer(models.Model):
+    c_ssn = models.IntegerField(primary_key=True)
+    c_name = models.CharField(max_length=50, null = False)
+    c_sex = models.CharField(max_length=10, null = False)
+    c_phone = models.CharField(max_length=10, null = False)
+    c_address = models.CharField(max_length=50, null = False)
+
+    class Meta:
+            db_table = 'customer'
+
+
+class Medicine(models.Model):
+    m_id = models.IntegerField(primary_key=True)
+    m_name = models.CharField(max_length=100, null = False)
+    m_price = models.IntegerField(null = False)
+    m_expiration_date = models.DateField(null = False)
+    e_ssn = models.IntegerField()
+    c_ssn = models.IntegerField()
+    p_id = models.IntegerField()
+    f_id = models.IntegerField()
+    purchase_date = models.DateField()
+
+    class Meta:
+            db_table = 'medicine'
+
+
+
+class Prescription_Medicine(models.Model):
+    m_id = models.IntegerField(primary_key=True)
+    d_id = models.IntegerField()
+
+    class Meta:
+            db_table = 'prescription_medicine'
+
+
+class See(models.Model):
+    d_id = models.IntegerField()
+    c_ssn = models.IntegerField()
+
+    class Meta:
+            db_table = 'see'
+
+
+class Contect(models.Model):
+    p_id = models.IntegerField()
+    f_id = models.IntegerField()
+
+    class Meta:
+        db_table = 'contect'
+
--- pharmacy_project/pharmacy_project/views.py
+++ pharmacy_project/pharmacy_project/views.py
@@ -13,12 +13,44 @@ def showPharmacyEmpolyee(request):
     str = ' and '
     if request.method == 'POST':
         employee_name = request.POST.get('employee-name')
+        pharmacy_name = request.POST.get('pharmacy-name')
         print(type(employee_name))
         if employee_name != '':
             sql = sql + str + "employee.e_name = '" + employee_name + "'"
+        if pharmacy_name != '':
+            sql = sql + str + "pharmacy.p_name = '" + pharmacy_name + "'"
     print(sql)
     #sql = "SELECT * FROM pharmacy, employee WHERE employee.p_id = pharmacy.p_id"
     cursor = connection.cursor()
     cursor.execute(sql)
     results = cursor.fetchall()
     return render(request, 'test.html', {'data':results})
+
+
+def tradeHistroyView(request):
+    sql = "SELECT * FROM medicine, prescription_medicine, customer, employee, doctor WHERE employee.e_ssn = medicine.e_ssn and customer.c_ssn = medicine.c_ssn"
+    str = ' and '
+    if request.method == 'POST':
+        customer_name = request.POST.get('customer-name')
+        employee_name = request.POST.get('employee-name')
+        medicine_name = request.POST.get('medicine-name')
+        date_later_than = request.POST.get('date-later-than')
+        date_before = request.POST.get('date-before')
+        #print(type(employee_name))
+        if customer_name != '':
+            sql = sql + str + "customer.c_name = '" + customer_name + "'"
+        if employee_name != '':
+            sql = sql + str + "employee.e_name = '" + employee_name + "'"
+        if medicine_name != '':
+            sql = sql + str + "medicine.m_name = '" + medicine_name + "'"
+        if date_later_than != '':
+            sql = sql + str + "medicine.purchase_date >= '" + date_later_than + "'"
+        if date_before != '':
+            sql = sql + str + "medicine.purchase_date <= '" + date_before + "'"
+    #print(sql)
+    #sql = "SELECT * FROM pharmacy, employee WHERE employee.p_id = pharmacy.p_id"
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    return render(request, 'trade_history.html', {'data':results})
+
